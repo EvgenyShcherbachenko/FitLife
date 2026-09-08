fe605b6f12816b3db276cb7cf6977e3fbb38adf3
Evgeniy Shcherbachenko <e.shcherbachenko@icloud.com>
2026-09-08 11:20:03 +0300

    доработка по результатам ревью 2

--- fit_life.py
+++ fit_life.py
@@ -1,35 +1,35 @@
 # Проект FitLife - MVP версия 1.0
 
+# 1. Константы
+WATER_ML_PER_KG = 30
+ML_IN_LITER = 1000
 
-# 1. Знакомство
+# 2. Знакомство
 print("Доброго времени суток! Давайте познакомимся.")
 user_name = input("Введите свое имя: ").strip()
 print("Очень приятно", user_name, sep=", ")
 try:
     user_age = int(input("Укажите свой возраст: "))
 except ValueError:
-    print("Ошибка: возраст нужно указать целым числом. Пример: 36")
+    raise ValueError("Ошибка: возраст нужно указать целым числом. Пример: 36")
 
-# 2. Сбор данных
+# 3. Сбор данных
 try:
     user_weight = float(input("Укажите свой вес в кг: "))
 except ValueError:
-    print("Ошибка: вес нужно указать числом.")
-
+    raise ValueError("Ошибка: вес нужно указать числом.")
 try:
     user_height = float(input("Укажите свой рост в метрах (пример: 1.77): "))
 except ValueError:
-    print("Ошибка: рост нужно указать числом и использовать точку")
+    raise ValueError("Ошибка: рост нужно указать числом и использовать точку.")
 print("Отлично! Теперь рассчитаем")
 
-# 3. Логика расчетов
-WATER_ML_PER_KG = 30
-ML_IN_LITER = 1000
-bmi = round((user_weight / (user_height ** 2)), 1)
+# 4. Логика расчетов
+bmi = round(user_weight / (user_height ** 2), 1)
 water_ml = user_weight * WATER_ML_PER_KG
 water_l = water_ml / ML_IN_LITER
 
-# 4. Вывод красивого результата
+# 5. Вывод результата
 print(f"Отчет для пользователя: {user_name} ({user_age} г.)")
 print(f"Твой Индекс Массы Тела: {bmi}")
 print(f"Рекомендуемая норма воды: {water_l:.2f} л. в день")
